80e388ad2de36ddcee720a73494b1c64f8abd2c5
DiuDiuDangAr <35565184+DiuDiuDangAr@users.noreply.github.com>
2019-12-20 14:24:39 +0800

    test modfiy

--- convert_xlsx_to_csv.py
+++ convert_xlsx_to_csv.py
@@ -3,6 +3,8 @@ import pandas as pd
 def convert_xlsx_to_csv():
 	df = pd.read_excel("內部財產.xlsx")
 	df.to_csv("內部財產複本.csv", sep=",", encoding="big5hkscs")
+	print("test")
+	#okok
 
 if __name__ == '__main__':
 	convert_xlsx_to_csv()
